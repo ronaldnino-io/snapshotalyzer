1cf75c5bff110e3477b7a566f71c416783bb89c6
Ronald Niño <ronaldnino.io@gmail.com>
2020-05-01 13:40:05 -0400

    Prueba de cambio de editor por de fecto de git. Realizando commit de dos lineas.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # snapshotalyzer
 Demo project to manage AWS EC2 instance snapshots
+Proyecto demo que nos permite manejar instancias en AWS EC2
+
+## Acerca del proyecto
+
+Este proyecto es un demo que  usa boto3 de python para administrar instancia AWS EC2.
+
+## Configuración
+
+Crear un  perfil de autenticación para cuenta de aws con el nombre shotty en el AWS CLI.
+
+##Ejecutar el script
+
+`python main.py`
---
+++ main.py
@@ -0,0 +1,17 @@
+#
+#Alyzer script tiene como fin automatizar la interacción con instancias ec2
+#Autor: Ronald Niño
+#
+
+#Importamos el kit de desarrollo de AWS
+import boto3
+
+#Iniciamos una sesión con una cuenta de AWS
+session =  boto3.Session(profile_name='shotty')
+
+#Nos conectamos al servicio de aec2
+ec2 =  session.resource('ec2')
+
+#Visualizamos todas las instancias que tenemo en ec2
+for instance in ec2.instances.all():
+    print(instance)
