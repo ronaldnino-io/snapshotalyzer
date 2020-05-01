dc2ed4424d598c6da4aef3636e8735711f56ace9
Ronald Niño <ronaldnino.io@gmail.com>
2020-05-01 16:34:22 -0400

    Version 2 script administracion de instancias con boto3

--- main.py
+++ main.py
@@ -5,14 +5,28 @@
 
 #Importamos el kit de desarrollo de AWS
 import boto3
+import sys
+import click
 
-if __name__ == '__main__':
-    #Iniciamos una sesión con una cuenta de AWS
-    session =  boto3.Session(profile_name='shotty')
+ #Iniciamos una sesión con una cuenta de AWS
+session =  boto3.Session(profile_name='shotty')
 
-    #Nos conectamos al servicio de aec2
-    ec2 =  session.resource('ec2')
+#Nos conectamos al servicio de aec2
+ec2 =  session.resource('ec2')
 
-    #Visualizamos todas las instancias que tenemo en ec2
+#Funcion que nos lista las instancias de una cuenta en AWS
+@click.command()
+def list_instances():
+    "list EC2 instances"
     for instance in ec2.instances.all():
-        print(instance)
+        print(', '.join((
+            instance.id,
+            instance.instance_type,
+            instance.placement['AvailabilityZone'],
+            instance.state['Name'],
+            instance.public_dns_name)))
+    return        
+
+   
+if __name__ == '__main__':
+    list_instances()
